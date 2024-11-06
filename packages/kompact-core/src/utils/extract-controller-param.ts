2aa3bbfcf2a826cbc666deb797f0acb3208afb8e
Trần Quang Sáng <tranquangsang12.7@gmail.com>
2024-11-06 17:25:11 +0700

    Feature: Add

--- packages/kompact-core/src/utils/extract-controller-param.ts
+++ packages/kompact-core/src/utils/extract-controller-param.ts
@@ -20,16 +20,23 @@ export const extractReqParams = (
     route.methodName
   );
 
-  paramMetadata?.forEach(({ index, name }: { index: number; name: string }) => {
-    // incase if name is undefined = @Param() params: object
-    args[index] = req.params[name] || req.params;
-  });
-  bodyMetadata?.forEach(
-    ({ index, field }: { index: number; field: string }) => {
-      // incase if name is undefined = @Body() body: object
-      args[index] = req.body[field] || req.body;
-    }
-  );
+  if (paramMetadata) {
+    paramMetadata.forEach(
+      ({ index, name }: { index: number; name: string }) => {
+        // incase if name is undefined = @Param() params: object
+        args[index] = req.params[name] || req.params;
+      }
+    );
+  }
+
+  if (bodyMetadata) {
+    bodyMetadata.forEach(
+      ({ index, field }: { index: number; field: string }) => {
+        // incase if name is undefined = @Body() body: object
+        args[index] = req.body[field] || req.body;
+      }
+    );
+  }
 
   if (Array.isArray(args) && args.length > 0) {
     instanceController[route.action.name](...args, res);
